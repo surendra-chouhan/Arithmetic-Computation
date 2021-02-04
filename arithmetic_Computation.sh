2d9e2cba1e35e63faca7e2013e7fdaa1ca5f6724
surendra-chouhan <chouhansurendra028@gmail.com>
2021-02-04 19:52:19 +0530

    Refactored the code to the Computation Result in the

--- arithmetic_Computation.sh
+++ arithmetic_Computation.sh
@@ -6,16 +6,12 @@ read -p "Enter 3 numbers : " a b c;
 echo $a $b $c
 
 Query1=`echo - | awk '{printf "%.2f\n", '$a+$b*$c'}'`
-echo $Query1
 
 Query2=`echo - | awk '{printf "%.2f\n", '$a*$b+$c'}'`
-echo $Query2
 
 Query3=`echo - | awk '{printf "%.2f\n", '$c+$a/$b'}'`
-echo $Query3
 
 Query4=`echo - | awk '{printf "%.2f\n", '$a%$b+$c'}'`
-echo $Query4
 
 declare -A Computation
 Computation[a+b*c]=$Query1
@@ -23,11 +19,6 @@ Computation[a*b+c]=$Query2
 Computation[c+a/b]=$Query3
 Computation[a%b+c]=$Query4
 
-echo "Results of all the computation are : " 
-for k in ${!Computation[@]}
-do
-	echo $k " : " ${Computation[$k]}
-done
 
 counter=1
 for i in ${!Computation[@]}
@@ -35,4 +26,11 @@ do
 	Result_Values[counter++]=${Computation[$i]}
 done
 
-echo ${Result_Values[@]}
+echo "Results of all the computation are : " ${Result_Values[@]}
+
+
+echo "Computation Result in the Descending Order is : "
+for k in ${!Computation[@]}
+do
+	echo $k ":" ${Computation[$k]}
+done | sort -nr -k3
