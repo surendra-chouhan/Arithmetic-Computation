52537211bb94ef876adbac433d4ff2b999e516a5
surendra-chouhan <chouhansurendra028@gmail.com>
2021-02-04 19:01:49 +0530

    Stored the results of all Computation in a Dictionary

--- arithmetic_Computation.sh
+++ arithmetic_Computation.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 echo "Welcome to Arithmetic Computation and Sorting Program!"
 
@@ -6,13 +6,25 @@ read -p "Enter 3 numbers : " a b c;
 echo $a $b $c
 
 Query1=`echo - | awk '{printf "%.2f\n", '$a+$b*$c'}'`
-echo $Query1
+#echo $Query1
 
 Query2=`echo - | awk '{printf "%.2f\n", '$a*$b+$c'}'`
-echo $Query2
+#echo $Query2
 
 Query3=`echo - | awk '{printf "%.2f\n", '$c+$a/$b'}'`
-echo $Query3
+#echo $Query3
 
 Query4=`echo - | awk '{printf "%.2f\n", '$a%$b+$c'}'`
-echo $Query4
+#echo $Query4
+
+declare -A Computation
+Computation[a+b*c]=$Query1
+Computation[a*b+c]=$Query2
+Computation[c+a/b]=$Query3
+Computation[a%b+c]=$Query4
+
+echo "Results of all the computation are : " 
+for k in ${!Computation[@]}
+do
+	echo $k " : " ${Computation[$k]}
+done
